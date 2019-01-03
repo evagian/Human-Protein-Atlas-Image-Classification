49a2671adde9635256ac899ff3d7da7fcad6b843
Eva Giannatou <eva_giannatou@yahoo.com>
2019-01-03 23:39:28 +0200

    Update 1_extract_green_images.py

--- 1_extract_green_images.py
+++ 1_extract_green_images.py
@@ -1,8 +1,8 @@
 
 import zipfile
 
-my_zip =  zipfile.ZipFile('./dataset/train.zip') # Specify your zip file's name here
-storage_path = './dataset/train'
+my_zip =  zipfile.ZipFile('../dataset/train.zip') # Specify your zip file's name here
+storage_path = '../dataset/train'
 for file in my_zip.namelist():
     if my_zip.getinfo(file).filename.endswith('_green.png'):
         my_zip.extract(file, storage_path) # extract the file to current folder if it is a text file
